91b344c85522ce354f25a35b7b004a170269d10c
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2024-11-24 20:58:33 +0500

    adding questions and showing answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./COMPONENTS/Header";
+import Quiz from "./COMPONENTS/Quiz";
 
 function App() {
-    return(
-        <>
-            <Header />
-        </>
-      
-    );
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/COMPONENTS/Quiz.jsx
+++ src/COMPONENTS/Quiz.jsx
@@ -5,9 +5,26 @@ export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([0]);
   const activeQuestionIndex = userAnswer.length;
 
+  function handleSelectAnswer(selectedAnswer){
+    setUserAnswer( (prevUserAnswer)=>{
+      return [...prevUserAnswer,selectedAnswer]
+
+    })
+
+  }
+
   return (
     <div id="question">
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {
+          QUESTIONS[activeQuestionIndex].answers.map((answer) =>(
+            <li key={answer} className="answer">
+              <button onClick={()=> handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }

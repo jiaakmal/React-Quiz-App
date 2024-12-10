4115a22fa191626061ec8d12b202a861bfc75419
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2024-12-10 21:09:03 +0500

    update question file

--- src/COMPONENTS/Questions.jsx
+++ src/COMPONENTS/Questions.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
-
+import QUESTIONS from '../questions'
 export default function Questions({
+  key,
   questionText,
   answers,
   onSelectAnswer,
@@ -10,6 +11,21 @@ export default function Questions({
   answerState,
   onSkipAnswers,
 }) {
+  [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+  const handleSelectAnswer = (answer) => {
+    setAnswer({ selectedAnswer: answer, isCorrect: null });
+  };
+  setTimeout(() => {
+    setAnswer({ selectedAnswer: answer, isCorrect: QUESTIONS[key].answers[0] === answer });
+
+  }, 1000);
+  let answerState = '';
+  if (answer.selectedAnswer) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong'
+  }
   return (
     <div id="question">
       <QuestionTimer timeout={10000} onTimeOut={onSkipAnswers} />
@@ -17,7 +33,7 @@ export default function Questions({
       <Answers
         answers={answers}
         selectedAnswer={selectedAnswer}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
         answerState={answerState}
       />
     </div>
